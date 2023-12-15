40655245a00a81a72b946806fc0b45d9ef81b316
tajbirahmed <tajbir26@gmail.com>
2023-12-15 16:06:39 +0600

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 export default function Home() { 
   return (
     <div className="w-full bg-[var(--Light-gray)] min-h-screen flex justify-center items-center">
-      <div className=" max-w-[400px] bg-[var(--White)] max-h-[600px] flex flex-col justify-center items-start rounded-2xl p-4">
+      <div className=" max-w-[380px] bg-[var(--White)] max-h-[600px] flex flex-col justify-center items-start rounded-2xl p-4">
         <div className="w-full border-8 border-[#3785ff] rounded-xl">
           <Image 
             src="/image-qr-code.png"
-            width={400}
-            height={400}
+            width={340}
+            height={340}
             alt="qr-code"
           />
         </div>
         <div className="flex flex-col justify-center items-center text-center">
           <h1 className="text-[28px] font-semibold p-4"> Improve your front-end skills by building projects</h1>
-          <p className="text-[15px] font-normal">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level </p>
+          <p className="text-[20px] font-extralight">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level </p>
         </div>
 
       </div>
